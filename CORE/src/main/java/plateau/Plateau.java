9102a9d746a2b944489099a5a225f4d7f4294ed6
Jacques Patané <patane.jacques@gmail.com>
2019-02-08 15:29:09 +0100

    Fix Plateau implemented methods

--- CORE/src/main/java/plateau/Plateau.java
+++ CORE/src/main/java/plateau/Plateau.java
@@ -1,13 +1,15 @@
 package plateau;
 
-import entites.AbstractEntite;
+import agent.AbstractAgent;
 import agent.AbstractAgentSitue;
+import common.Direction;
+import entites.AbstractEntite;
 
 import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
-public class Plateau implements IEntitePlateau, IDeveloppeurPlateau {
+public class Plateau implements IEntitePlateau, IDeveloppeurPlateau, IAgentPlateau {
 
     private String nom ;
     private Map<AbstractAgentSitue, Case> listeAgents ;
@@ -29,17 +31,37 @@ public class Plateau implements IEntitePlateau, IDeveloppeurPlateau {
     }
 
     @Override
-    public Boolean placerAgent(Point point, AbstractAgentSitue agent) {
+    public Boolean placerAgentite(Point point, IAgentite agentite) {
         return null;
     }
 
     @Override
-    public Boolean placerEntite(Point point, AbstractEntite agent) {
+    public IAgentite enleverAgentite(Point point, IAgentite agentite) {
         return null;
     }
 
     @Override
-    public AbstractEntite enleverEntite(Point point, AbstractEntite entite) {
+    public Boolean deplacerAgent(AbstractAgent agent, Direction direction) {
+        return null;
+    }
+
+    @Override
+    public Boolean ramasserEntite(AbstractAgent agent, AbstractEntite entite) {
+        return null;
+    }
+
+    @Override
+    public Boolean deposerEntite(AbstractAgent agent, AbstractEntite entite) {
+        return null;
+    }
+
+    @Override
+    public Map<Direction, Case> getVoisinage(AbstractAgent agent) {
+        return null;
+    }
+
+    @Override
+    public Case getCase(AbstractAgent agent) {
         return null;
     }
 }
