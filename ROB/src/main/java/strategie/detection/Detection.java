f24c7a519daffd66c4a9667b0db31156ea04dc76
celinemorin <celine.morin.42@gmail.com>
2019-02-21 12:00:07 +0100

    debut detection

---
+++ ROB/src/main/java/strategie/detection/Detection.java
@@ -0,0 +1,17 @@
+package strategie.detection;
+
+import entites.CaseRobot;
+
+import java.util.List;
+
+public class Detection {
+
+    public static void detecterVoisinage(List<List<CaseRobot>> carteMemoire) {
+
+    }
+
+    private void addCaseRobotToCarteMemoire(List<List<CaseRobot>> carteMemoire, CaseRobot caseRobot) {
+
+    }
+
+}
