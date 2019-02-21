d9ba6d8eb31d3766e759eb17db4027d5c1144c76
laetitiatureau <IntelliJ6>
2019-02-21 11:59:52 +0100

    add package initialisationPlateau and class GenerateurPlateau

---
+++ ROB/src/main/java/initialisationPlateau/GenerateurPlateau.java
@@ -0,0 +1,12 @@
+package initialisationPlateau;
+
+import entites.Zone;
+import plateau.Position;
+
+public class GenerateurPlateau {
+
+    public Zone initZone(Position case1, Position case2) {
+        //TODO
+        return null;
+    }
+}
