792d74afb37111db31f35146eab80effb961a4df
Thibault <thibault.nougues@hotmail.fr>
2019-03-05 20:54:36 +0100

    Mise à jour de la démo core

--- CORE/src/main/java/demo/EntiteActiveDemo.java
+++ CORE/src/main/java/demo/EntiteActiveDemo.java
@@ -3,17 +3,29 @@ package demo;
 import entites.AbstractEntiteActive;
 
 public class EntiteActiveDemo extends AbstractEntiteActive {
+
+    private int value;
+
     /**
      * Constructeur par défaut.
      *
      * @param nom nom de l'agent
      */
-    public EntiteActiveDemo(String nom) {
+    public EntiteActiveDemo(String nom, int value) {
         super(nom);
+        this.value = value % 3;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value % 3;
     }
 
     @Override
     public void ActionTour() {
-
+        this.value = (this.value + 1) % 3;
     }
 }
--- CORE/src/main/java/demo/Main.java
+++ CORE/src/main/java/demo/Main.java
@@ -7,6 +7,7 @@ import plateau.IAgentite;
 import plateau.Position;
 
 import java.awt.*;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Main {
@@ -14,6 +15,7 @@ public class Main {
     public static void main(final String[] args) {
         //Instance de Application
         Application application = new Application("test", 20, 20);
+        application.setCaseSize(30);
 
         //region Initialisation du plateau
         application.placerAgentite(new Position(17, 6), new Obstacle("testObstacle"));
@@ -27,26 +29,47 @@ public class Main {
         agent3.setDirection(Direction.SE);
         application.placerAgentite(new Position(1, 1), agent3);
 
-        AgentDemo agent4 = new AgentDemo(application.getIAgentPlateau());
-        agent4.setDirection(Direction.NE);
-        application.placerAgentite(new Position(3, 3), agent4);
-
-        application.placerAgentite(new Position(3, 3), new EntiteActiveDemo("activeTest"));
+        application.placerAgentite(new Position(3, 3), new EntiteActiveDemo("activeTest", 0));
+        application.placerAgentite(new Position(8, 3), new EntiteActiveDemo("activeTest", 2));
 
         application.placerAgentite(new Position(10, 10), new EntitePasiveDemo("passiveTest"));
         //endregion
 
         //region Personalisation de l'affichage
         application.setCasePaint((caseToPaint, graphics, positionX, positionY, sizeMax) -> {
-            Stream<IAgentite> iAgentiteStream = caseToPaint.getAgentites().stream().filter(aCase -> aCase instanceof AgentDemo);
-            if (iAgentiteStream.count() > 0) {
+            Supplier<Stream<IAgentite>> iAgentiteStream = () -> caseToPaint.getAgentites().stream().filter(aCase -> aCase instanceof AgentDemo);
+            if (iAgentiteStream.get().count() > 0) {
                 graphics.setColor(Color.GREEN);
                 graphics.fillRect(positionX, positionY, sizeMax, sizeMax);
+                iAgentiteStream.get().forEach(iAgentite -> {
+                    if (((AgentDemo) iAgentite).getEntitePortee() != null) {
+                        graphics.setColor(Color.GRAY);
+                        graphics.fillOval(positionX + 3, positionY + 3, sizeMax - 6, sizeMax - 6);
+                    }
+                });
             }
-            iAgentiteStream = caseToPaint.getAgentites().stream().filter(aCase -> aCase instanceof EntitePasiveDemo);
-            if (iAgentiteStream.count() > 0) {
+            iAgentiteStream = () -> caseToPaint.getAgentites().stream().filter(aCase -> aCase instanceof EntitePasiveDemo);
+            if (iAgentiteStream.get().count() > 0) {
                 graphics.setColor(Color.RED);
                 graphics.fillOval(positionX + 3, positionY + 3, sizeMax - 6, sizeMax - 6);
+            }
+            iAgentiteStream = () -> caseToPaint.getAgentites().stream().filter(aCase -> aCase instanceof EntiteActiveDemo);
+            if (iAgentiteStream.get().count() > 0) {
+                iAgentiteStream.get().forEach(iAgentite -> {
+                    Color color;
+                    switch (((EntiteActiveDemo) iAgentite).getValue()) {
+                        case 0:
+                            graphics.setColor(Color.BLUE);
+                            break;
+                        case 1:
+                            graphics.setColor(Color.CYAN);
+                            break;
+                        default:
+                            graphics.setColor(Color.black);
+                            break;
+                    }
+                    graphics.fillRoundRect(positionX + 3, positionY + 3, sizeMax - 6, sizeMax - 6, 5, 5);
+                });
             }
         });
         //endregion
